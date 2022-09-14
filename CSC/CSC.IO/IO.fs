faf9c82e8966875003f772e2ceb77fc2aff0c634
Uri Keselman <ukeselman@crossriverbank.com>
2022-09-14 10:49:58 +0300

    Server config

--- CSC/CSC.IO/IO.fs
+++ CSC/CSC.IO/IO.fs
@@ -2,31 +2,43 @@
 
 open System.IO
 open Blockchain
+open Wallet
 
 [<AutoOpen>]
 module IO =
     
-    let readBlock i = 
-        let path = Path.Combine(Directory.GetCurrentDirectory(), "Blockchain", (sprintf "%i.dat" i))
+    let private DATA_DIR = "Data"
+
+    let readBlock path i = 
+        let path = Path.Combine(path, DATA_DIR, (sprintf "%i.dat" i))
         if File.Exists(path) then
             File.ReadAllText(path) |> Some
         else
             None
 
-    let writeBlock i b =
-        let path = Path.Combine(Directory.GetCurrentDirectory(), "Blockchain", (sprintf "%i.dat" i))
+    let writeBlock path i b =
+        let path = Path.Combine(path, DATA_DIR, (sprintf "%i.dat" i))
         File.WriteAllText(path, b)
 
-    let saveBlock block i =
+    let saveBlock path block i =
         block
         |> Serializer.serialize 
-        |> writeBlock i
+        |> writeBlock path i
 
-    let loadBlockchain =
+    let loadBlockchain path =
         let rec loadBlock blocks i =
-            match readBlock i with
+            match readBlock path i with
             | Some stream -> 
                 let block = Serializer.deserialize<Block> stream 
                 loadBlock (block :: blocks) (i + 1)
             | _ -> blocks
-        loadBlock [] 0
+        loadBlock [] 1
+
+    let loadWallet path =
+        load (fun name -> System.IO.File.ReadAllText(name)) Serializer.deserialize<Wallet.Wallet> path
+
+    let saveWallet path wallet =
+        Wallet.save 
+            (fun w -> async { File.WriteAllText(Path.Combine(path, wallet.name), w) }) 
+            Serializer.serialize 
+            wallet
--- CSC/CSC/Program.fs
+++ CSC/CSC/Program.fs
@@ -12,63 +12,7 @@ open Suave.WebSocket
 open Suave.Operators
 open Suave.Sockets
 open Suave.Sockets.Control
-open System.IO
-
-
-//Wallet.save (fun w -> File.WriteAllText(wallet.name, w)) Serializer.serialize wallet
-
-//let wallet2 = 
-//    Wallet.load (fun name -> File.ReadAllText(name)) Serializer.deserialize<Wallet.Wallet> wallet.name
-
-
-
-//let block = Blockchain.tryCreateBlock None [ tx ] time 1 1UL
-
-//let rec mine blocks threshold nonce lim =
-//    let time = unixTime DateTime.Now
-//    let tx = Wallet.createCoinbaseTransaction 100UL time key
-//    let prevBlock = blocks |> List.tryHead
-//    match Blockchain.tryCreateBlock prevBlock [ tx ] time threshold nonce with
-//    | Some block -> 
-//        if lim > (blocks |> List.length) then mine (block :: blocks) threshold nonce lim
-//        else (block :: blocks)
-//    | _ -> mine blocks threshold (Blockchain.nextNonce nonce) lim
-
-//let blocks = mine [] 1 1UL 10
-
-//blocks
-//|> List.iteri
-//    (fun i b ->
-//        Blockchain.save 
-//            (fun s -> 
-//                let path = Path.Combine(Directory.GetCurrentDirectory(), "Blockchain", (sprintf "%i.dat" i))
-//                File.WriteAllText(path, s)
-//            ) 
-//            b
-//    )
-
-//let blocks = loadBlockchain
-//let newBlock =
-//    Miner.mine 
-//        key
-//        blocks
-//        DateTime.Now
-//        1
-//        1UL
-
-//match newBlock with
-//| Some block -> saveBlock block ((blocks |> List.length) + 1)
-//| _ -> ()
-    //Blockchain.load 
-    //    (fun i ->
-    //        let path = Path.Combine(Directory.GetCurrentDirectory(), "Blockchain", (sprintf "%i.dat" i))
-    //        if File.Exists(path) then
-    //            File.ReadAllText(path) |> Some
-    //        else
-    //            None
-    //    )
-
-let mutable continueLooping = true
+open System.Configuration
 
 type Command =
     | GetBalance of Key
@@ -91,15 +35,6 @@ let parseCommand (cmd: string)  =
     | tp :: name :: _ when tp = "newwallet" -> NewWallet name
     | _ -> Undefined
 
-let txcmd (cmd:string) =
-    //tx from to amount
-    let parts = cmd.Split(' ')
-    let mutable amount: uint64 = 0UL
-    if parts |> Array.length = 4 && parts.[0] = "tx" && UInt64.TryParse(parts.[3], &amount) then
-        Some (Convert.FromBase64String(parts.[1]), Convert.FromBase64String(parts.[2]), amount)
-    else
-        None
-
 //ukeselman
 //09V9BX8ikB7P/CDFXWghDWKD/uyrmOLsPdt2bWiS+bU=
 //A1QOL+SG0FJcxqABrE58PO2as1CeO6eAsv9yCJclNE0s
@@ -111,78 +46,86 @@ let txcmd (cmd:string) =
 //18446744073709551615UL
 //4073709551615UL
 
-let mutable server: Server = Server(saveBlock, Miner(), 744073709551615UL)
+let mutable server: Server = 
+    Server(
+        saveBlock ConfigurationManager.AppSettings.["Storage"], 
+        Miner(), 
+        Convert.ToUInt64(ConfigurationManager.AppSettings.["Threshold"]))//744073709551615UL
 
-let ws (webSocket : WebSocket) (context: HttpContext) =
+let ws (webSocket : WebSocket) _ =
     socket {
         let mutable loop = true
         while loop do
-            let! msg = webSocket.read()
-            match msg with
-            | (Text, data, true) ->
-                match data |> UTF8.toString |> parseCommand with
-                | GetBalance key -> 
-                    let response =
-                        key 
-                        |> createPubKeyBytes
-                        |> server.GetBalance
-                        |> (sprintf "balance %i")
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
-                    do! webSocket.send Text response true
+            try 
+                let! msg = webSocket.read()
+                match msg with
+                | (Text, data, true) ->
+                    match data |> UTF8.toString |> parseCommand with
+                    | GetBalance key -> 
+                        let response =
+                            key 
+                            |> createPubKeyBytes
+                            |> server.GetBalance
+                            |> (sprintf "balance %i")
+                            |> System.Text.Encoding.UTF8.GetBytes
+                            |> ByteSegment
+                        do! webSocket.send Text response true
             
-                | Pay (from, to_, amount) ->
-                    let response = 
-                        match server.Pay from to_ amount with
-                        | Ok _ -> sprintf "payok"
-                        | Error e -> sprintf "payerr %s" e
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
-                    do! webSocket.send Text response true
+                    | Pay (from, to_, amount) ->
+                        let response = 
+                            match server.Pay from to_ amount with
+                            | Ok _ -> sprintf "payok"
+                            | Error e -> sprintf "payerr %s" e
+                            |> System.Text.Encoding.UTF8.GetBytes
+                            |> ByteSegment
+                        do! webSocket.send Text response true
                 
-                | GetTransactions key ->
-                    let response = 
-                        key
-                        |> createPubKeyBytes
-                        |> server.GetTransactions
-                        |> List.sortByDescending (fun t -> t.time)
-                        |> List.map Blockchain.describeUserTransaction
-                        |> (fun list -> String.Join(" ", list |> List.toArray))
-                        |> sprintf "transactions %s" 
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
-                    do! webSocket.send Text response true
+                    | GetTransactions key ->
+                        //for outgoing - return transactions and not inputs
+                        //don't pay change when 0
+                        let response = 
+                            key
+                            |> createPubKeyBytes
+                            |> server.GetTransactions
+                            |> List.sortByDescending (fun t -> t.time)
+                            |> List.map Blockchain.describeUserTransaction
+                            |> (fun list -> String.Join(" ", list |> List.toArray))
+                            |> sprintf "transactions %s" 
+                            |> System.Text.Encoding.UTF8.GetBytes
+                            |> ByteSegment
+                        do! webSocket.send Text response true
 
-                | Pubkey key ->
-                    let response = 
-                        key
-                        |> createPubKeyBytes
-                        |> (fun k -> Convert.ToBase64String(k))
-                        |> sprintf "pubkey %s" 
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
-                    do! webSocket.send Text response true
+                    | Pubkey key ->
+                        let response = 
+                            key
+                            |> createPubKeyBytes
+                            |> (fun k -> Convert.ToBase64String(k))
+                            |> sprintf "pubkey %s" 
+                            |> System.Text.Encoding.UTF8.GetBytes
+                            |> ByteSegment
+                        do! webSocket.send Text response true
 
-                | NewWallet name ->
-                    let wallet = { Wallet.name = name; Wallet.key = createPrivateKeyBytes }
-                    Async.Start <| 
-                        Wallet.save 
-                            (fun w -> async { File.WriteAllText(wallet.name, w) }) 
-                            Serializer.serialize 
-                            wallet
-                    let response = 
-                        sprintf "wallet %s %s" wallet.name (Convert.ToBase64String(wallet.key))
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
-                    do! webSocket.send Text response true
+                    | NewWallet name ->
+                        let wallet = { Wallet.name = name; Wallet.key = createPrivateKeyBytes }
+                        Async.RunSynchronously <| saveWallet ConfigurationManager.AppSettings.["Storage"] wallet
+                        let response = 
+                            sprintf "wallet %s %s" wallet.name (Convert.ToBase64String(wallet.key))
+                            |> System.Text.Encoding.UTF8.GetBytes
+                            |> ByteSegment
+                        do! webSocket.send Text response true
 
+                    | _ -> ()
+
+                | (Close, _, _) ->
+                    let emptyResponse = [||] |> ByteSegment
+                    do! webSocket.send Close emptyResponse true
+                    loop <- false
                 | _ -> ()
-
-            | (Close, _, _) ->
-                let emptyResponse = [||] |> ByteSegment
-                do! webSocket.send Close emptyResponse true
-                loop <- false
-            | _ -> ()
+            with
+            | (exc: Exception) ->
+                printfn "%s" (exc.Message)
+                let response = exc.Message |> sprintf "error %s" |> System.Text.Encoding.UTF8.GetBytes |> ByteSegment
+                do! webSocket.send Close response true
     }
 
 let app: WebPart =
@@ -190,52 +133,29 @@ let app: WebPart =
         path "/websocket" >=> handShake ws
     ]
 
+let mutable continueLooping = true
+
 [<EntryPoint>]
 let main _ =
-    let wallet = load (fun name -> System.IO.File.ReadAllText(name)) Serializer.deserialize<Wallet.Wallet> "ukeselman"
-    let minerKey = wallet.key
+    try
+        let minerWallet = loadWallet ConfigurationManager.AppSettings.["MinerWallet"]
+        let blockchain = loadBlockchain ConfigurationManager.AppSettings.["Storage"]
 
-    Async.Start <| server.Start minerKey
-    Async.Start <| async { startWebServer defaultConfig app }
+        server.InitBlocks blockchain
+        Async.Start <| server.Start minerWallet.key
+        Async.Start <| async { startWebServer defaultConfig app }
     
-
-    //let k = createPrivateKeyBytes
-    //let pk = createPubKeyBytes k
-    //let wallet = { Wallet.name="ukeselman2";Wallet.keys=[k;pk] }
-    //Wallet.save (fun w -> System.IO.File.WriteAllText(wallet.name, w)) Serializer.serialize wallet
-
-    while continueLooping do
-        match Console.ReadLine() with
-        | "q" -> 
-            continueLooping <- false
-            server.Stop
-        | _ -> 
-            let t = "balance A1QOL+SG0FJcxqABrE58PO2as1CeO6eAsv9yCJclNE0s"
-            let cmd = parseCommand t
-            match cmd with
-            | GetBalance k -> 
-                printfn "%i" (server.GetBalance k)
-            | _ -> ()
-            //"tx 09V9BX8ikB7P/CDFXWghDWKD/uyrmOLsPdt2bWiS+bU= AtvsszMjn1tMD5Xb+cpKglgw3hBg1tVoWFrdomW6/Mbq 10" 
-            //match txcmd t with
-            //| Some (from, to_, amount) ->
-            //    let pubkey = createPubKeyBytes from
-            //    match Client.tryPay pubkey amount with
-            //    | Ok (utxos, total) -> 
-            //        let inputs =
-            //            utxos
-            //            |> List.choose (createTransactionInput from)
-            //        let output =
-            //            createTransactionOutput to_ amount
-            //        let change =
-            //            createTransactionOutput pubkey (total - amount)
-            //        let tx = createTransaction inputs [output; change] (unixTime DateTime.Now)
-
-            //        Client.pay tx
-
-            //    | Error e -> printfn "%s" e
-            //| _ -> ()
-
-    
-
+        while continueLooping do
+            match Console.ReadLine() with
+            | "q" -> 
+                continueLooping <- false
+                server.Stop
+            | _ -> 
+                ()
+            
+    with 
+    | (exc: Exception) -> 
+        printfn "%s" (exc.Message)
+        printfn "Press any key to exit."
+        Console.ReadLine() |> ignore
     0
